7186678db10f04a81613d5293ec471d6b66045c0
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-04-24 23:51:01 +0200

    feat: draft document sheet and update css for print

--- src/app/components/document/document.component.ts
+++ src/app/components/document/document.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-document',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './document.component.html',
   styleUrl: './document.component.css',
 })
